15c5a9cd421de9ba1160ce4b4225fce613b6c73e
Essmann <kenwilliam1000@gmail.com>
2025-07-14 10:37:10 +0200

    Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,19 @@ function Header({setNumCards, settings, currentPlayer, score, showHint, resetGam
     var value = input.target.value;
     setNumCards(value);
 }  
+    useEffect(() => {
+  const wrapper = document.querySelector(".card_container_wrapper");
+  if (!wrapper) return;
+
+  wrapper.classList.remove("turn-1", "turn-2");
+
+  if (currentPlayer === 1) {
+    wrapper.classList.add("turn-1");
+  } else if (currentPlayer === 2) {
+    wrapper.classList.add("turn-2");
+  }
+}, [currentPlayer]);
+
     var row = Math.floor(window.innerWidth / (50 + 10*2));
      var col = Math.floor(window.innerHeight / (75 + 10*2));
 
@@ -23,8 +36,7 @@ function Header({setNumCards, settings, currentPlayer, score, showHint, resetGam
             {currentPlayer == 2 ? <div className='scoreCount turn red' id='player_2_score'>{score[2]}</div> : <div className='scoreCount' id='player_2_score'>{score[2]}</div>}
             {currentPlayer == 1 ? <div className='scoreCount turn blue' id='player_1_score'>{score[1]}</div> : <div className='scoreCount ' id='player_1_score'>{score[1]}</div>}
             <br></br>
-            <div className={`currentPlayerTitle ${currentPlayer == 2 ? "red" : "blue"}`}>{currentPlayer==2 ? "Red" : "Blue"}</div>
-            <button onClick={showHint}>Show hint</button>
+            
             <IconBxRefresh width={24} height={24} id='iconBx' onClick={resetGame} />
             <Hamburger  toggled={dropDownOpen} toggle={setDropDownOpen} />
             
